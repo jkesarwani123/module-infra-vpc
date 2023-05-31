27c3535999ce439fe4430b1eaf62ea0dc912f0e9
Juhi K <jkesarwani.us@gmail.com>
2023-05-30 23:04:59 -0400

    module-VPC

--- main.tf
+++ main.tf
@@ -2,3 +2,12 @@ resource "aws_vpc" "main" {
   cidr_block = var.cidr_block
   tags = merge(var.tags, { Name = "${var.env}-vpc"})
 }
+
+resource "aws_subnet" "main" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "Main"
+  }
+}
