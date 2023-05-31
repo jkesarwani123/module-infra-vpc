e375cdc7d502a95c15c62113d41b64a2dbcd3a53
Juhi K <jkesarwani.us@gmail.com>
2023-05-31 12:30:56 -0400

    module-subnets

--- subnets/main.tf
+++ subnets/main.tf
@@ -3,6 +3,6 @@ resource "aws_subnet" "main" {
   vpc_id     = var.vpc_id
   cidr_block = var.cidr_block[count.index]
 
-  tags = merge(var.tags, { Name = "${env}-${var.name}-${count.index}"})
+  tags = merge(var.tags, { Name = "${var.env}-${var.name}-subnet-${count.index + 1}"})
 
 }
