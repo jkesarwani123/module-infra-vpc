8abcb85d1dec16050eb0ee55a2112e78b1e837c7
Juhi K <jkesarwani.us@gmail.com>
2023-05-31 19:28:05 -0400

    module-NAT gateway

--- main.tf
+++ main.tf
@@ -1,8 +1,10 @@
+# Create one VPC
 resource "aws_vpc" "main" {
   cidr_block = var.cidr_block
   tags = merge(var.tags, { Name = "${var.env}-vpc"})
 }
 
+# Create 2 subnets each for public, web, app and db
 module "subnets" {
   source = "./subnets"
   for_each = var.subnets
@@ -14,16 +16,19 @@ module "subnets" {
   env = var.env
 }
 
+# Create one Internet Gateway
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.main.id
   tags = merge(var.tags, { Name = "${var.env}-igw"})
 }
 
+# Allocate Elastic IP for NAT Gateway
 resource "aws_eip" "ngw" {
   count = length(var.subnets["public"].cidr_block)
   tags  = merge(var.tags, { Name = "${var.env}-ngw" })
 }
 
+# Create 2 NAT Gateways for the 2 public subnets
 resource "aws_nat_gateway" "ngw" {
   count         = length(var.subnets["public"].cidr_block)
   allocation_id = aws_eip.ngw[count.index].id
